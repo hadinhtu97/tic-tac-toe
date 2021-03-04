6008ea463a598dffef36fb0b36cf8a30afffe3b6
Tu Ha Dinh <53150506+hadinhtu97@users.noreply.github.com>
2021-03-04 23:42:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Next template
+
+## Description
+
+
+## Use
+```
+git clone https://github.com/hadinhtu97/abc
+cd abc
+npm install
+npm run build
+npm run start
+```
+
+## Demo 
+[Link demo](https://abc.hadinhtu97.repl.co/)
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+
+const Home = () => {
+    return (
+        <>
+            <Head>
+                <title>Next temp</title>
+            </Head>
+            <main className={styles.main}>
+
+            </main>
+        </>
+    )
+}
+
+export default Home
